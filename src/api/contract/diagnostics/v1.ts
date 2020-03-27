ade2067c9d80d537cd78f8bd6ecac4f8d5720603
itowlson <github@hestia.cc>
2020-03-27 17:25:11 +1300

    Experimental ideas for more concise authoring

--- src/api/contract/diagnostics/v1.ts
+++ src/api/contract/diagnostics/v1.ts
@@ -3,9 +3,11 @@
 // at all times.
 
 import * as vscode from 'vscode';
+import * as kp from 'k8s-manifest-parser';
 
 export interface DiagnosticsV1 {
     registerDiagnosticsContributor(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor): void;
+    registerDiagnosticsContributor2(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor2): void;
 }
 
 export namespace DiagnosticsV1 {
@@ -13,5 +15,49 @@ export namespace DiagnosticsV1 {
         readonly name: string;
         analyse(document: vscode.TextDocument): vscode.Diagnostic[] | IterableIterator<vscode.Diagnostic> | Promise<vscode.Diagnostic[]> | Promise<IterableIterator<vscode.Diagnostic>>;
         codeActions?(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export interface DocumentDiagnoser {
+        readonly name: string;
+        analyseDocument(document: vscode.TextDocument): vscode.Diagnostic[] | IterableIterator<vscode.Diagnostic> | Promise<vscode.Diagnostic[]> | Promise<IterableIterator<vscode.Diagnostic>>;
+        codeActions?(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export interface ResourceParsesDiagnoser {
+        readonly name: string;
+        analyseResourceParses(parses: ReadonlyArray<kp.ResourceParse>): EasyMode[] | IterableIterator<EasyMode> | Promise<EasyMode[]> | Promise<IterableIterator<EasyMode>>;
+        // TODO: vscode.CodeActionContext may not work well here because it provides a vscode.Diagnostic which has a vscode.Range.  But maybe that's okay as we have a kp.Range as well.
+        // TODO: do we need the VS Code TextDocument for URIing etc.?
+        codeActions?(parses: ReadonlyArray<kp.ResourceParse>, range: kp.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export interface ResourceParseDiagnoser {
+        readonly name: string;
+        readonly manifestKind?: string;
+        analyseResourceParse(parse: kp.ResourceParse): EasyMode[] | IterableIterator<EasyMode> | Promise<EasyMode[]> | Promise<IterableIterator<EasyMode>>;
+        codeActions?(parse: kp.ResourceParse, range: kp.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export interface ResourceDiagnoser {
+        readonly name: string;
+        readonly manifestKind?: string;
+        analyseResource(resource: kp.MapTraversalEntry): EasyMode[] | IterableIterator<EasyMode> | Promise<EasyMode[]> | Promise<IterableIterator<EasyMode>>;
+        codeActions?(parse: kp.ResourceParse, range: kp.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export interface ResourceParseEvaluatorDiagnoser {
+        readonly name: string;
+        readonly manifestKind?: string;
+        readonly evaluator: kp.ResourceEvaluator<EasyMode>;
+        codeActions?(parse: kp.ResourceParse, range: kp.Range, context: vscode.CodeActionContext): Promise<(vscode.Command | vscode.CodeAction)[]>;
+    }
+
+    export type DiagnosticsContributor2 = DocumentDiagnoser | ResourceParsesDiagnoser | ResourceParseDiagnoser | ResourceParseEvaluatorDiagnoser | ResourceDiagnoser;
+
+    export interface EasyMode {
+        readonly range: kp.Range;
+        readonly message: string;
+        readonly severity?: vscode.DiagnosticSeverity;
+        readonly code?: string | number;
     }
 }
--- src/api/implementation/diagnostics/v1.ts
+++ src/api/implementation/diagnostics/v1.ts
@@ -1,7 +1,10 @@
 import * as vscode from 'vscode';
+import * as kp from 'k8s-manifest-parser';
 
 import { DiagnosticsV1 } from '../../contract/diagnostics/v1';
 import { registerLinter, Linter } from '../../../components/lint/linters';
+import { cantHappen } from '../../../utils/never';
+import { flatten } from '../../../utils/array';
 
 export function impl(refresh: (document: vscode.TextDocument) => {}): DiagnosticsV1 {
     return new DiagnosticsV1Impl(refresh);
@@ -12,6 +15,11 @@ class DiagnosticsV1Impl implements DiagnosticsV1 {
 
     registerDiagnosticsContributor(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor): void {
         const linter = asLinter(diagnosticContributor);
+        registerLinter(linter, this.refresh);
+    }
+
+    registerDiagnosticsContributor2(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor2): void {
+        const linter = asLinter2(diagnosticContributor);
         registerLinter(linter, this.refresh);
     }
 }
@@ -35,5 +43,152 @@ function asLinter(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor): 
         name,
         lint,
         codeActions
+    };
+}
+
+function isDocumentDiagnoser(o: DiagnosticsV1.DiagnosticsContributor2): o is DiagnosticsV1.DocumentDiagnoser {
+    return !!((o as DiagnosticsV1.DocumentDiagnoser).analyseDocument);
+}
+
+function isResourceParsesDiagnoser(o: DiagnosticsV1.DiagnosticsContributor2): o is DiagnosticsV1.ResourceParsesDiagnoser {
+    return !!((o as DiagnosticsV1.ResourceParsesDiagnoser).analyseResourceParses);
+}
+
+function isResourceParseDiagnoser(o: DiagnosticsV1.DiagnosticsContributor2): o is DiagnosticsV1.ResourceParseDiagnoser {
+    return !!((o as DiagnosticsV1.ResourceParseDiagnoser).analyseResourceParse);
+}
+
+function isResourceDiagnoser(o: DiagnosticsV1.DiagnosticsContributor2): o is DiagnosticsV1.ResourceDiagnoser {
+    return !!((o as DiagnosticsV1.ResourceDiagnoser).analyseResource);
+}
+
+function isResourceParseEvaluatorDiagnoser(o: DiagnosticsV1.DiagnosticsContributor2): o is DiagnosticsV1.ResourceParseEvaluatorDiagnoser {
+    return !!((o as DiagnosticsV1.ResourceParseEvaluatorDiagnoser).evaluator);
+}
+
+function asLinter2(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor2): Linter {
+    function name() { return diagnosticContributor.name; }
+
+    return {
+        name,
+        lint: makeLintFunction(diagnosticContributor),
+        codeActions: async function (_document: vscode.TextDocument, _range: vscode.Range, _context: vscode.CodeActionContext) { return []; }
+    };
+}
+
+function makeLintFunction(diagnosticContributor: DiagnosticsV1.DiagnosticsContributor2): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    if (isDocumentDiagnoser(diagnosticContributor)) {
+        return makeDocumentDiagnoserLintFunction(diagnosticContributor);
+    }
+    if (isResourceParsesDiagnoser(diagnosticContributor)) {
+        return makeResourceParsesDiagnoserLintFunction(diagnosticContributor);
+    }
+    if (isResourceParseDiagnoser(diagnosticContributor)) {
+        return makeResourceParseDiagnoserLintFunction(diagnosticContributor);
+    }
+    if (isResourceDiagnoser(diagnosticContributor)) {
+        return makeResourceDiagnoserLintFunction(diagnosticContributor);
+    }
+    if (isResourceParseEvaluatorDiagnoser(diagnosticContributor)) {
+        return makeResourceParseEvaluatorDiagnoserLintFunction(diagnosticContributor);
+    }
+    return cantHappen(diagnosticContributor);
+}
+
+function arrayOf<T>(items: T[] | IterableIterator<T>): T[] {
+    if (Array.isArray(items)) {
+        return items;
+    }
+    return Array.of(...items);
+}
+
+function makeDocumentDiagnoserLintFunction(diagnosticContributor: DiagnosticsV1.DocumentDiagnoser): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    return async function(document: vscode.TextDocument) {
+        const diagnostics = await diagnosticContributor.analyseDocument(document);
+        return arrayOf(diagnostics);
+    };
+}
+
+function makeResourceParsesDiagnoserLintFunction(diagnosticContributor: DiagnosticsV1.ResourceParsesDiagnoser): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    return async function analyseImpl(document: vscode.TextDocument): Promise<vscode.Diagnostic[]> {
+        function rangeOf(r: kp.Range) {
+            return new vscode.Range(document.positionAt(r.start), document.positionAt(r.end));
+        }
+        const text = document.getText();
+        const parses = (document.languageId === 'yaml' ? kp.parseYAML(text) :
+                       (document.languageId === 'json' ? kp.parseJSON(text) :
+                        []));
+        if (parses.length === 0) {
+            return [];
+        }
+
+        const diags = await diagnosticContributor.analyseResourceParses(parses);
+        return arrayOf(diags).map((d) => { const diag = new vscode.Diagnostic(rangeOf(d.range), d.message, d.severity); diag.code = d.code; return diag; });
+    };
+}
+
+function makeResourceParseDiagnoserLintFunction(diagnosticContributor: DiagnosticsV1.ResourceParseDiagnoser): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    return async function analyseImpl(document: vscode.TextDocument): Promise<vscode.Diagnostic[]> {
+        function rangeOf(r: kp.Range) {
+            return new vscode.Range(document.positionAt(r.start), document.positionAt(r.end));
+        }
+        const text = document.getText();
+        const parses = (document.languageId === 'yaml' ? kp.parseYAML(text) :
+                       (document.languageId === 'json' ? kp.parseJSON(text) :
+                        []));
+
+        const resources = diagnosticContributor.manifestKind ? parses.filter((p) => kp.isKind(p, diagnosticContributor.manifestKind!)) : parses;
+        if (resources.length === 0) {
+            return [];
+        }
+
+        const diagPromises = resources.map((r) => diagnosticContributor.analyseResourceParse(r));
+        const diagsArray = await Promise.all(diagPromises);
+        const diagsArray2 = diagsArray.map((c) => arrayOf(c));
+        const diags = flatten(...diagsArray2);
+        return diags.map((d) => { const diag = new vscode.Diagnostic(rangeOf(d.range), d.message, d.severity); diag.code = d.code; return diag; });
+    };
+}
+
+function makeResourceDiagnoserLintFunction(diagnosticContributor: DiagnosticsV1.ResourceDiagnoser): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    return async function analyseImpl(document: vscode.TextDocument): Promise<vscode.Diagnostic[]> {
+        function rangeOf(r: kp.Range) {
+            return new vscode.Range(document.positionAt(r.start), document.positionAt(r.end));
+        }
+        const text = document.getText();
+        const parses = (document.languageId === 'yaml' ? kp.parseYAML(text) :
+                       (document.languageId === 'json' ? kp.parseJSON(text) :
+                        [])).map((p) => kp.asTraversable(p));
+
+        const resources = diagnosticContributor.manifestKind ? parses.filter((p) => kp.isKind(p, diagnosticContributor.manifestKind!)) : parses;
+        if (resources.length === 0) {
+            return [];
+        }
+
+        const diagPromises = resources.map((r) => diagnosticContributor.analyseResource(r));
+        const diagsArray = await Promise.all(diagPromises);
+        const diagsArray2 = diagsArray.map((c) => arrayOf(c));
+        const diags = flatten(...diagsArray2);
+        return diags.map((d) => { const diag = new vscode.Diagnostic(rangeOf(d.range), d.message, d.severity); diag.code = d.code; return diag; });
+    };
+}
+
+function makeResourceParseEvaluatorDiagnoserLintFunction(diagnosticContributor: DiagnosticsV1.ResourceParseEvaluatorDiagnoser): (document: vscode.TextDocument) => Promise<vscode.Diagnostic[]> {
+    return async function analyseImpl(document: vscode.TextDocument): Promise<vscode.Diagnostic[]> {
+        function rangeOf(r: kp.Range) {
+            return new vscode.Range(document.positionAt(r.start), document.positionAt(r.end));
+        }
+        const text = document.getText();
+        const parses = (document.languageId === 'yaml' ? kp.parseYAML(text) :
+                       (document.languageId === 'json' ? kp.parseJSON(text) :
+                        []));
+
+        const resources = diagnosticContributor.manifestKind ? parses.filter((p) => kp.isKind(p, diagnosticContributor.manifestKind!)) : parses;
+        if (resources.length === 0) {
+            return [];
+        }
+
+        const diags = kp.evaluate(resources, diagnosticContributor.evaluator);
+        return diags.map((d) => { const diag = new vscode.Diagnostic(rangeOf(d.range), d.message, d.severity); diag.code = d.code; return diag; });
     };
 }
